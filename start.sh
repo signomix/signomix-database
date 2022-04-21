e51337c830dd377075b727e234b39f2a237b0d25
Grzegorz Skorupa <GSkorupa@nmg.pl>
2022-04-21 13:38:41 +0200

    wip

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/iot -user sa -script /h2/init.sql
-java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/data -user sa -script /h2/init.sql
 java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/iotdata -user sa -script /h2/init.sql
-java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/actuator -user sa -script /h2/init.sql
 java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/auth -user sa -script /h2/init.sql
 java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/user -user sa -script /h2/init.sql
 java -cp h2-1.4.200.jar org.h2.tools.RunScript -url jdbc:h2://h2data/cms -user sa -script /h2/init.sql
